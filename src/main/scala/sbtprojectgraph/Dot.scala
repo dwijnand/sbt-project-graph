27d643f3e05573f10bcb6b7888dc217cb85d6969
Sebastian Wiesner <sebastian@swsnr.de>
2018-05-28 12:51:32 +0200

    Sort nodes and edges
    Make DOT output stable.

--- src/main/scala/sbtprojectgraph/Dot.scala
+++ src/main/scala/sbtprojectgraph/Dot.scala
@@ -10,8 +10,8 @@ object Dot {
   }
 
   def toFileContent(nodes: Seq[ResolvedProject], edges: Seq[Edge[ResolvedProject]]): String = {
-    val nodesContent = nodes map (n => s"""    "${n.id}"[label=<${n.id}>]""") mkString "\n"
-    val edgesContent = edges map (e => s"""    "${e.from.id}" -> "${e.to.id}" ${edgeStyle(e)}""") mkString "\n"
+    val nodesContent = nodes.map(n => s"""    "${n.id}"[label=<${n.id}>]""").sorted.mkString("\n")
+    val edgesContent = edges.map(e => s"""    "${e.from.id}" -> "${e.to.id}" ${edgeStyle(e)}""").sorted.mkString("\n")
     val content =
       s"""
          |digraph "projects-graph" {
